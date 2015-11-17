0e5514ad2a13c279d6f4c247511c2755da7a8227
Korrawe Karunratanakul <korrawe@gmail.com>
2015-11-17 15:15:19 +0700

    Major Change in Main
    - Main now works as Game Manager
    - Added methods to switch scene

--- Dragoniquarium/src/main/Main.java
+++ Dragoniquarium/src/main/Main.java
@@ -1,16 +1,28 @@
 package main;
 
+import input.InputUtility;
+
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import render.GameScreen1;
+import render.GameTitle;
+import render.GameWindow;
 import logic.GameLogic;
 
 public class Main {
 
+	private static GameTitle titleScene;
+	private static GameScreen1 gameScreen;
+	private static GameWindow gameWindow;
+	private static JComponent nextScene = null;
+	
 	public static void main(String[] args) {
 		
-		JFrame frame = new JFrame("Dragoniquarium");
+		
+		
+		/*JFrame frame = new JFrame("Dragoniquarium");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		GameLogic logic = new GameLogic();
@@ -29,7 +41,46 @@ public class Main {
 			gameScreen.repaint();
 			logic.logicUpdate();
 			input.InputUtility.postUpdate();
+		}*/
+		
+		titleScene = new GameTitle();
+		
+		GameLogic logic = new GameLogic();
+		/*JComponent*/ gameScreen = new GameScreen1();
+		
+		gameWindow = new GameWindow(titleScene);
+		
+		
+		
+		while(true){
+			try {
+				Thread.sleep(20);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			gameWindow.getCurrentScene().repaint();
+			if(gameWindow.getCurrentScene() instanceof GameScreen1){
+				logic.logicUpdate();
+				InputUtility.postUpdate();
+			}
+			if(nextScene != null){
+				if(gameWindow.getCurrentScene() instanceof GameScreen1)
+					logic.onExit();
+				gameWindow.switchScene(nextScene);
+				if(nextScene instanceof GameScreen1)
+					logic.onStart();
+				nextScene = null;
+			}
 		}
+	}
+	
+	public static void goToTitle(){
+		nextScene = titleScene;
+	}
+	
+	public static void newGame(){
+		nextScene = gameScreen;
+		if(gameScreen == null) System.out.println("null");
 	}
 
 }
